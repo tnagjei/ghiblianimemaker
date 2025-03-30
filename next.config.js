3caabcbd007a72d992ac0867618a09e04c23915d
tnagjei <132260476+tnagjei@users.noreply.github.com>
2025-03-30 23:19:00 +0800

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+    domains: [
+      "source.unsplash.com",
+      "images.unsplash.com",
+      "ext.same-assets.com",
+      "ugc.same-assets.com",
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ext.same-assets.com',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
